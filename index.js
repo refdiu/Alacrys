b2a021d1f209bd80c0d637fba591cc8f4fb5267b
refdiu <lahirishraman@gmail.com>
2026-06-27 06:42:09 +0530

    adding intro para for readers

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const dropdown = document.getElementById("dropdown");
 const topbar = document.getElementById("topbar");
+const arrow = document.getElementById("ima")
 
 dropdown.addEventListener("click", function() {
     topbar.classList.toggle("open");
 });
+
+window.onclick = function(event){
+    if(event.target == arrow){
+        arrow.style.transform = "rotate(180deg)";
+    }
+}
