de642a7865dda38d589baa58076e17826aca8694
refdiu <lahirishraman@gmail.com>
2026-07-03 01:30:55 +0530

    better backgrounds

--- characters/vanilla-tilt.js
+++ characters/vanilla-tilt.js
@@ -503,14 +503,7 @@ return VanillaTilt;
 
 const dropdown = document.getElementById("dropdown");
 const topbar = document.getElementById("topbar");
-const arrow = document.getElementById("ima")
 
 dropdown.addEventListener("click", function() {
     topbar.classList.toggle("open");
 });
-
-window.onclick = function(event){
-    if(event.target == arrow){
-        arrow.style.transform = "rotate(180deg)";
-    }
-}
