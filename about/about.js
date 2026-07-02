12298bb760087bb9b83c095685339f786b085eea
refdiu <lahirishraman@gmail.com>
2026-07-02 20:58:26 +0530

    waiting server

---
+++ about/about.js
@@ -0,0 +1,14 @@
+const dropdown = document.getElementById("dropdown");
+const topbar = document.getElementById("topbar");
+const arrow = document.getElementById("ima")
+
+dropdown.addEventListener("click", function() {
+    topbar.classList.toggle("open");
+});
+
+window.onclick = function(event){
+    if(event.target == arrow){
+        arrow.style.transform = "rotate(180deg)";
+    }
+}
+
